77dd7a75bb87c430c6af09d04273ab1dfd22efcf
JeffreyKirigo <jeffreyem99@gmail.com>
2022-08-23 03:13:27 +0000

    Hello, write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
 stderr_file = sys.stderr
-stderr_file.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+stderr_file.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
 sys.exit(1)
