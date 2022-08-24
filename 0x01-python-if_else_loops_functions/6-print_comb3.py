9b0ebc6eecceab492bc830d679725c696cf1e4ae
JeffreyKirigo <jeffreyem99@gmail.com>
2022-08-24 12:35:20 +0000

    Combine 3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 10):
-    for sec_num in range(num + 1,10):
+    for sec_num in range(num + 1, 10):
         if num == 8 and sec_num == 9:
             print("{}{}".format(num, sec_num))
         else:
