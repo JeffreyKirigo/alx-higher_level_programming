19140d2eb46e349bc18cb882c21b828c949031b7
JeffreyKirigo <jeffreyem99@gmail.com>
2022-08-31 10:33:20 +0000

    Removes all c's from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    str_cp = [x for x in my_string if x != 'c' or x != 'C']
-    return ("".join(str_cp))
+    str_cp = my_string.translate({ord(x): None for x in 'cC'})
+    return str_cp
