f19748a7e8a7c7121714ad445346df94886d211b
JeffreyKirigo <jeffreyem99@gmail.com>
2022-09-20 20:07:43 +0000

    Rename attribute

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Represent a rectangle."""
 
-    counter = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -13,7 +13,7 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-        type(self).counter += 1
+        type(self).number_of_instances += 1
 
         self.width = width
         self.height = height
