5e60d6e6032eeadd1a6082ba80e45ec31bdeecf3
JeffreyKirigo <jeffreyem99@gmail.com>
2022-09-26 10:18:08 +0000

    Raise a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """raises a name exception with a message.
+
+    Args:
+        message (str): Message to be passed
+    
+    Returns:
+        Nothing
+    """
+    raise NameError(message)
