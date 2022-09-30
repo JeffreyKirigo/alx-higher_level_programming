ecae3edcdcf4e68a31b5373f3a67fc39b9fcfdc2
JeffreyKirigo <jeffreyem99@gmail.com>
2022-09-30 10:09:30 +0000

    Print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,13 +12,13 @@ def print_square(size):
         Nothing
     """
     symbol = "#"
-    if not isinstance(size,int):
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
-    elif isinstance(size,float) and size < 0:
+    elif isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     else:
         for i in range(size):
-            [print("{}".format(symbol),end="") for k in range(size)]
+            [print("{}".format(symbol), end="") for k in range(size)]
             print()
