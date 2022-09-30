61c80c2b91d7fec35249a161b687534cf2ec3f6d
JeffreyKirigo <jeffreyem99@gmail.com>
2022-09-30 09:16:40 +0000

    Documentation conventions

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,15 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """Prints the named parameters
+
+    Args:
+        first_name (str): first name as string
+        last_name (str): last name as string
+
+    Returns:
+        Nothing
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
