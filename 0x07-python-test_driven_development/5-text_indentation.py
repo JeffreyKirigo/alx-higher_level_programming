6a07ba5e8b2817848ea57f9e85f807b46d4df6c0
JeffreyKirigo <jeffreyem99@gmail.com>
2022-10-01 13:54:05 +0000

    Text Indent

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -24,7 +24,7 @@ def text_indentation(text):
                 print("\n")
         else:
             if x == " ":
-                checker = False
+                checker = True
                 continue
             else:
                 print(x, end="")
