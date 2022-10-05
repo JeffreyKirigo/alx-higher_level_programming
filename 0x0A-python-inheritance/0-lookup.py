72cd9f87891b43c2896c2397bb2907556bad3c0b
JeffreyKirigo <jeffreyem99@gmail.com>
2022-10-05 15:09:37 +0000

    Lookup methods and att

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define lookup"""
+
+
+def lookup(obj):
+    """Attributes and methods of an object
+
+    Args:
+        obj (object): Object to check for attributes
+    Returns:
+        attributes and methods of an object"""
+    return dir(ob)
