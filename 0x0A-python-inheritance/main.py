c437efde0805c9a41266df14cf3e46c7bcc393e0
JeffreyKirigo <jeffreyem99@gmail.com>
2022-10-05 16:39:58 +0000

    Code refactor

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-is_kind_of_class = __import__('3-is_kind_of_class').is_kind_of_class
+is_kind_of_class = __import__('is_kind_of_class').is_kind_of_class
 
 a = 1
 if is_kind_of_class(a, int):
