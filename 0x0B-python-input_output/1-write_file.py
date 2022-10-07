c1441ef6314c6ec1468c0f1033cff6b9499cef19
JeffreyKirigo <jeffreyem99@gmail.com>
2022-10-07 05:07:24 +0000

    Write and count

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 """Define method"""
 
+
+def write_file(filename="", text=""):
+    """Write to file
+    Args:
+        filename (str): name of file
+        text (str): Content to append to file
+    """
+    with open(filename, mode="w", encoding="utf-8") as m_file:
+        return m_file.write(text)
