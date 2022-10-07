a4bfddfc9e4cfc69c965005522c9f0873ad09d1e
JeffreyKirigo <jeffreyem99@gmail.com>
2022-10-07 04:36:00 +0000

    Readfile

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define method"""
+
+
+def read_file(filename=""):
+    """Reads a file and prints to stdout
+    Args:
+        filename (str): name of file to read
+    """
+    with open(filename, encoding="utf-8") as m_file:
+        print(m_file.read())
