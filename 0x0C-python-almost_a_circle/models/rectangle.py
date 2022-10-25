2477af76a92234a3148522c477ea0703863df196
JeffreyKirigo <jeffreyem99@gmail.com>
2022-10-25 04:15:39 +0000

    First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,11 +13,6 @@ class Rectangle(Base):
         __y -> y
     """
 
-    __width -> width
-    __height -> height
-    __x -> x
-    __y -> y
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """Define init method"""
         super().__init__(id)
@@ -30,23 +25,27 @@ class Rectangle(Base):
     def width(self):
         """getter"""
         return self.__width
+
     @width.setter
     def width(self, value):
         """setter"""
         self.__width = value
+
     @width.deleter
     def width(self):
         """deleter"""
         del self.__width
-    
+
     @property
     def height(self):
         """getter"""
         return self.__height
+
     @height.setter
     def height(self, value):
         """Setter"""
         self.__height = value
+
     @height.deleter
     def height(self):
         """Deleter"""
