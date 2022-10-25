a8fec6944f0e6610338f31f702d5ecad7b811d71
JeffreyKirigo <jeffreyem99@gmail.com>
2022-10-25 04:12:20 +0000

    First Rect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,6 +12,12 @@ class Rectangle(Base):
         __x -> x
         __y -> y
     """
+
+    __width -> width
+    __height -> height
+    __x -> x
+    __y -> y
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Define init method"""
         super().__init__(id)
@@ -19,3 +25,29 @@ class Rectangle(Base):
         self.__height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """getter"""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """setter"""
+        self.__width = value
+    @width.deleter
+    def width(self):
+        """deleter"""
+        del self.__width
+    
+    @property
+    def height(self):
+        """getter"""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """Setter"""
+        self.__height = value
+    @height.deleter
+    def height(self):
+        """Deleter"""
+        del self.__height
