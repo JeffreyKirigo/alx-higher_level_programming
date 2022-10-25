bd197ca8d804db10337df9b28c1d22e40d4ecc47
JeffreyKirigo <jeffreyem99@gmail.com>
2022-10-25 08:47:49 +0000

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,11 +15,12 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Define init method"""
-        super().__init__(id)
-        self.__width = width
-        self.__height = height
+
+        self.width = width
+        self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
@@ -29,16 +30,11 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """setter"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
-
-    @width.deleter
-    def width(self):
-        """deleter"""
-        del self.__width
 
     @property
     def height(self):
@@ -47,17 +43,11 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        """Setter"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
-
-    @height.deleter
-    def height(self):
-        """Deleter"""
-        del self.__height
 
     @property
     def x(self):
@@ -73,11 +63,6 @@ class Rectangle(Base):
             raise ValueError("x must be >= 0")
         self.__x = value
 
-    @x.deleter
-    def x(self):
-        """Deleter"""
-        del self.__x
-
     @property
     def y(self):
         """getter"""
@@ -91,8 +76,3 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
-
-    @y.deleter
-    def y(self):
-        """Deleter"""
-        del self.__y
