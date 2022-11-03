8f2a6d10b94238eeaf58145cb5d2e7f1954632d1
JeffreyKirigo <jeffreyem99@gmail.com>
2022-11-03 02:39:18 +0000

    Create, set password and grant all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
+-- Create, set password and grant all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
