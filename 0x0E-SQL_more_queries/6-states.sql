c4b5b635b9e80a32fa70cfd768c199f2dc957fdc
JeffreyKirigo <jeffreyem99@gmail.com>
2022-11-03 12:55:19 +0000

    State table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- CREATES table states in db hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256), PRIMARY KEY (id), UNIQUE KEY (id));
