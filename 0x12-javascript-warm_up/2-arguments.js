584bb7cf9ea5b3ed8f4d112b43b1744f920ab288
JeffreyKirigo <jeffreyem99@gmail.com>
2022-11-29 04:35:17 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,8 @@
 const len = process.argv.length;
 if (len < 3) {
   console.log('No argument');
-} else if (len == 3) {
+}
+if (len == 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
