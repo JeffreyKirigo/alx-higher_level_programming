4504dcf8dbbdabd0f16231fcabb7f3407241526b
JeffreyKirigo <jeffreyem99@gmail.com>
2022-11-29 04:17:42 +0300

    Arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const { argv } = require('node:process');
 
 if (argv.length < 3) {
-  console.log('No arguments');
+  console.log('No argument');
 } else if (argv.length == 3) {
   console.log('Argument found');
 } else {
