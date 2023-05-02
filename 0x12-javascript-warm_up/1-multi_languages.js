cfcc90150ec928e89a0df179fd243bfc9d715bfb
Jeffrey Kirigo <jeffreyem99@gmail.com>
2023-05-02 23:56:14 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log('C is fun');
-console.log('Python is cool');
-console.log(myVar);
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const args = process.argv;
-const parsed = parseInt(args[2]);
-if (isNaN(parsed)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + parsed);
-}
