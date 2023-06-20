6ac46b6617517a5c2c64e8706b32e1d30862559f
JeffreyKirigo <jeffreyem99@gmail.com>
2023-06-20 22:57:21 +0300

    Filter like N and Order By states id

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    List all states
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db_conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                              password=sys.argv[2], db=sys.argv[3],
+                              port=3306)
+
+    cur = db_conn.cursor()
+    cur.execute("""
+                SELECT * FROM states WHERE name like 'N%'
+                ORDER BY states.id
+                """)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db_conn.close()
