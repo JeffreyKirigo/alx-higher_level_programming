a6a319734bab02740cc40ce226bc64d43a2e7c65
JeffreyKirigo <jeffreyem99@gmail.com>
2023-06-20 23:51:40 +0300

    Pass it and find it

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    List all states and filter where name starts with N
+    Ordered by state ID.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db_conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                              password=sys.argv[2], db=sys.argv[3],
+                              port=3306)
+
+    cur = db_conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{find}'"
+               .format(find=sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db_conn.close()
