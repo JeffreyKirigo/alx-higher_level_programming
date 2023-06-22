dbccb61b52874e4d1264ab51df3b38686f6e1517
JeffreyKirigo <jeffreyem99@gmail.com>
2023-06-22 21:43:49 +0300

    Format: find

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                               port=3306)
 
     cur = db_conn.cursor()
-    query = "SELECT * FROM states WHERE name LIKE %s ORDER BY id"
+    query = "SELECT * FROM states WHERE name LIKE %s"
     find = "{}".format(sys.argv[4])
     cur.execute(query, (find, ))
     rows = cur.fetchall()
