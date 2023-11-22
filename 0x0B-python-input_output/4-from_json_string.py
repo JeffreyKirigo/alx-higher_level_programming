7313cf77bb32048899b7909cd3fa4df79a62d78d
Jeffrey Kirigo <jeffreyem99@gmail.com>
2023-11-23 01:16:57 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/4-from_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Define method"""
-import json
-
-
-def from_json_string(my_str):
-    """Returns from json to python object"""
-    return json.loads(my_str)
--- 0x0B-python-input_output/5-save_to_json_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Define method"""
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    """Returns from json to python object"""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
